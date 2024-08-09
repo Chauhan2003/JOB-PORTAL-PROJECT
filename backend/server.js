f85beceebbbd873881e46687e87d0ad2b4f2b67b
Gagan Chauhan <gagan2020chauhan@gmail.com>
2024-08-09 12:36:11 +0530

    done some cors configuration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware:-
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
